607cbb5a3684b9820ef33c6c0bbaf9d561f06738
Rahul Tuteja <39482980+tutu15o9@users.noreply.github.com>
2020-03-20 04:11:20 +0530

    Blog Website complete

--- 15. Todolist-v1/app.js
+++ 15. Todolist-v1/app.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const date = require(__dirname + "/date.js")
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -16,15 +16,7 @@ app.use(express.static("public"));
 let items = ["Buy food", "Cook Food", "Eat food"];
 let workItems = [];
 app.get("/", function(req, res) {
-  let today = new Date();
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long"
-  };
-  let day = today.toLocaleDateString("en-US", options);
-
-
+  day = date.getDate();
   res.render("list", {
     listTitle: day,
     newListItems: items
