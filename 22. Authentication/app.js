709b7bfc3d0d3cfd3b0d994e42cb0572fea8757b
Rahul Tuteja <39482980+tutu15o9@users.noreply.github.com>
2020-04-01 04:28:17 +0530

    Level 2 encryption

--- 22. Authentication/app.js
+++ 22. Authentication/app.js
@@ -3,6 +3,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+
 
 const app = express();
 
@@ -13,12 +15,15 @@ app.use(bodyParser.urlencoded({extended:true}));
 mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser:true,useUnifiedTopology:true});
 // mongoose.connect("mongodb://localhost:27017/userDb",{useUnifiedTopology:true, useNewUrlParser:true});
 
-const userScema = {
+const userSchema = new mongoose.Schema({
 	email:String,
 	password:String
-};
+});
 
-const User = new mongoose.model("User",userScema);
+const secret = "Thisisourlittlesecrer.";
+userSchema.plugin(encrypt,{secret: secret,encryptedFields:["password"]});
+
+const User = new mongoose.model("User",userSchema);
 app.get("/",function(req,res){
 	res.render("home");
 });
