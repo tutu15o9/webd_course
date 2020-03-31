972ab4ac01a13c25731e266bfe9f9866d7833157
Rahul Tuteja <39482980+tutu15o9@users.noreply.github.com>
2020-04-01 04:09:12 +0530

    Authentication Level 1

---
+++ 22. Authentication/app.js
@@ -0,0 +1,81 @@
+//jshint esversion:6
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+
+mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser:true,useUnifiedTopology:true});
+// mongoose.connect("mongodb://localhost:27017/userDb",{useUnifiedTopology:true, useNewUrlParser:true});
+
+const userScema = {
+	email:String,
+	password:String
+};
+
+const User = new mongoose.model("User",userScema);
+app.get("/",function(req,res){
+	res.render("home");
+});
+
+
+app.get("/login",function(req,res){
+	res.render("login");
+});
+
+app.post("/login",function(req,res){
+	const userName = req.body.username;
+	const password = req.body.password;
+
+	User.findOne({email:userName},function(err,founduser){
+		if (err) {
+			console.log(err);
+		} else {
+			if(founduser.password === password){
+				res.render("secrets");
+			}
+		}
+	});
+});
+
+
+app.get("/register",function(req,res){
+	res.render("register");
+});
+
+app.post("/register",function(req,res){
+	console.log(req.body.password);
+	console.log(req.body.username);
+	const newUser = new User({
+		email:req.body.username,
+		password:req.body.password
+	});
+
+	newUser.save(function(err){
+		if (err) {
+			console.log(err);
+		} else {
+			// console.log("ij");
+			res.render("secrets");
+		}
+	});
+
+
+});
+
+
+
+
+
+
+
+
+
+app.listen(3000,function(){
+	console.log("Server started on port 3000.");
+});
