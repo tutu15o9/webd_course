9becdac57ee943354432bf47bd3f7cec77074a70
Rahul Tuteja <39482980+tutu15o9@users.noreply.github.com>
2020-04-01 04:59:19 +0530

    "Level 3 Hashing password"

--- 22. Authentication/app.js
+++ 22. Authentication/app.js
@@ -1,11 +1,12 @@
 //jshint esversion:6
-require('dotenv').config();
+// require('dotenv').config();
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
-
+// const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
@@ -22,7 +23,7 @@ const userSchema = new mongoose.Schema({
 });
 
 // const secret = "Thisisourlittlesecrer.";
-userSchema.plugin(encrypt,{secret: process.env.SECRET,encryptedFieldscd :["password"]});
+// userSchema.plugin(encrypt,{secret: process.env.SECRET,encryptedFieldscd :["password"]});
 
 const User = new mongoose.model("User",userSchema);
 app.get("/",function(req,res){
@@ -36,13 +37,13 @@ app.get("/login",function(req,res){
 
 app.post("/login",function(req,res){
 	const userName = req.body.username;
-	const password = req.body.password;
+	const password = ( req.body.password);
 
 	User.findOne({email:userName},function(err,founduser){
 		if (err) {
 			console.log(err);
 		} else {
-			if(founduser.password === password){
+			if(founduser.password === md5(password)){
 				res.render("secrets");
 			}
 		}
@@ -55,11 +56,9 @@ app.get("/register",function(req,res){
 });
 
 app.post("/register",function(req,res){
-	console.log(req.body.password);
-	console.log(req.body.username);
 	const newUser = new User({
 		email:req.body.username,
-		password:req.body.password
+		password:md5(req.body.password)
 	});
 
 	newUser.save(function(err){
