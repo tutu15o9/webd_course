ec4168c18e8c4110db7ccc37cf5257b35bf99037
Rahul Tuteja <39482980+tutu15o9@users.noreply.github.com>
2020-05-22 18:53:25 +0530

    Keeper app part 2

--- 23. Keeper-app-part-1/src/Components/Note.jsx
+++ 23. Keeper-app-part-1/src/Components/Note.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-function Note() {
+function Note(params) {
   return (
     <div className="note">
-      <h1>This is the note title</h1>
-      <p>This is the note content</p>
+      <h1>{params.title}</h1>
+      <p>{params.content}</p>
     </div>
   );
 }
